762a3052b154d9bd129b6c45602adc2a250715ed
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-04-29 14:38:46 -0600

    Imagen y sección Body
    Se añadió una imagen en la parte de header y se creo una seccion para body

--- portafolio-app/src/pages/components/Header.tsx
+++ portafolio-app/src/pages/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 
 function Header() {
@@ -8,6 +9,7 @@ function Header() {
         <li>< a href="#">Perfil</a></li>
         <li>< a href="#">Proyectos</a></li>
         <li>< a href="#">Contacto</a></li>
+        <Image src = "https://static.vecteezy.com/system/resources/previews/004/550/083/non_2x/houses-logo-illustration-free-vector.jpg" width={200} height={200} alt="aaa"/>
       </div>
     );
   }

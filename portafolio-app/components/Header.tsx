e0d1dae0806c9b399f81999274b644a3ac5f7d31
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-04-29 15:57:16 -0600

    Cambio de carpeta
    para un mejor funcionamiento se cambio la carpeta y se creo un nuevo proyecto mejor ordenado para evitar errores

---
+++ portafolio-app/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import Logo from '../public/Logo Andrey.png'
+import styles from "../styles/Home.module.css"
+
+
+const Header = () => {
+    return (
+        <nav className={styles.nav}>
+            <button
+                type='button'>
+                <Image src={Logo} alt="" width={150} height={100} />
+            </button>
+
+            <li>< a href="#">Inicio</a></li>
+            <li>< a href="#">Informacion</a></li>
+            <li>< a href="#">Proyectos</a></li>
+            <li>< a href="#">Contrataciones</a></li>
+            <li>< a href="#">Contacto</a></li>
+
+        </nav>
+    )
+}
+
+export default Header

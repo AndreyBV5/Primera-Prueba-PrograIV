e5a4b14685e88f2983b1a01990de9f32a2b47b94
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-04-29 16:57:55 -0600

    Proyectos
    se crea apartado de proyectos

--- portafolio-app/pages/index.tsx
+++ portafolio-app/pages/index.tsx
@@ -2,7 +2,7 @@ import type {NextPage} from 'next'
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Header from '@/components/Header'
-import Hero from '@/components/hero'
+import Hero from '@/components/Hero'
 
 
 const Home: NextPage = () =>{

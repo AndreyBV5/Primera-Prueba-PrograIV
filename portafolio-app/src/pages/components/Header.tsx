95d65eb368453f2fe627947b9133206516a61190
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-04-29 13:57:44 -0600

    Menu
    se creó un menú para acceder a distintas secciones

---
+++ portafolio-app/src/pages/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Header() {
+    return (
+      <div>
+        <li>< a href="#">Inicio</a></li>
+        <li>< a href="#">Perfil</a></li>
+        <li>< a href="#">Proyectos</a></li>
+        <li>< a href="#">Contacto</a></li>
+      </div>
+    );
+  }
+
+  export default Header;

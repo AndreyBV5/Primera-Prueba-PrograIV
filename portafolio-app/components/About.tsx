8d5dacc97b5a5a3714a9ca1206b2b8d64839d31a
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-02 16:57:54 -0600

    new

--- portafolio-app/components/About.tsx
+++ portafolio-app/components/About.tsx
@@ -8,24 +8,26 @@ const About = () => {
 
         <div className={styles.about}>
             <figure>
-            <Image src={Profile} alt="" width={400} height={250} />
+                <Image src={Profile} alt="" width={400} height={250} />
             </figure>
-            <section id = "About">
-            <article>
-                <h2 className={styles.desktop}>
-                    I’m Andrey, and I like to
-                    <br />
-                    work on your next project
-                </h2>
-                <p className={styles.desktop}>
-                    I like working with others to create design solutions.
-                    <br />
-                    I have done some projects
-                    <br />
-                    most college jobs.
-                </p>
-                <button><a href="https://www.linkedin.com/in/andrey-barrios-valverde-16a707274?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B9QUWZsFPTxuJccjkYHJAJg%3D%3D" type="button">Free Consultation Linkedin</a></button>
-            </article>
+            <section id="About">
+                <article>
+                    <h2 className={styles.desktop}>
+                        I’m Andrey, and I like to
+                        <br />
+                        work on your next project
+                    </h2>
+                    <p className={styles.desktop}>
+                        I like working with others to create design solutions.
+                        <br />
+                        I have done some projects
+                        <br />
+                        most college jobs.
+                    </p>
+                    <a href="https://www.linkedin.com/in/andrey-barrios-valverde-16a707274?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B9QUWZsFPTxuJccjkYHJAJg%3D%3D">
+                        <button>Free Consultation Linkedin</button>
+                    </a>
+                </article>
             </section>
         </div>
     );

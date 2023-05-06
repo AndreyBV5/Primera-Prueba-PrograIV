2e9296e8eb68ebae9e32ea11f50eeeafdee30375
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-06 09:42:16 -0600

    Carrusel motion
    se regreso al disñeo anterior, no se pudo consumir la api

--- portafolio-app/components/Projects.tsx
+++ portafolio-app/components/Projects.tsx
@@ -2,108 +2,103 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import One from '../public/1.png'
+import Two from '../public/2.jpg'
+import Three from '../public/3.jpg'
+import Four from '../public/4.jpg'
+import Five from '../public/5.jpeg'
 
 const Projects = () => {
-  type Repo = {
-    id: number;
-    name: string;
-  }
   const [x, setX] = useState(0);
-  const [repos, setRepos] = useState<Repo[]>([]);
 
-  const handleSlide = (direction: any) => {
-    const slideWidth = window.matchMedia("(max-width: 700px)").matches
-      ? 285.066666
-      : 570.133333;
-    if (direction === "left") {
-      setX(x - slideWidth);
+  const handleSlide = (direction: string) => {
+    if (window.matchMedia("(max-width: 700px)").matches) {
+      if (direction === "left") {
+        setX(x - 285.066666);
+      } else {
+        setX(x + 285.066666);
+      }
     } else {
-      setX(x + slideWidth);
+      if (direction === "left") {
+        setX(x - 570.133333);
+      } else {
+        setX(x + 570.133333);
+      }
     }
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://api.github.com/users/AndreyBV5/repos"
-      );
-      const data = await response.json();
-      setRepos(data);
-    };
-    fetchData();
-  }, []);
+    if (window.matchMedia("(max-width: 700px)").matches) {
+      if (x > 570.133333) {
+        setX(570.133333);
+      }
+      if (x < -570.133333) {
+        setX(-570.133333);
+      }
+    } else {
+      if (x > 1140.26667) {
+        setX(1140.26667);
+      }
+      if (x < -1140.26667) {
+        setX(-1140.26667);
+      }
+    }
+  }, [x]);
 
-  useEffect(() => {
-    const slideWidth = window.matchMedia("(max-width: 700px)").matches
-      ? 285.066666
-      : 570.133333;
-    if (x > slideWidth * (repos.length - 1)) {
-      setX(slideWidth * (repos.length - 1));
-    }
-    if (x < -slideWidth * (repos.length - 1)) {
-      setX(-slideWidth * (repos.length - 1));
-    }
-  }, [x, repos]);
   return (
     <>
       <div className={styles.desktop}>
-        <section id="Projects">
-          <section className={styles.work}>
-            <h2>My Projects</h2>
-            <motion.div
-              animate={{ x }}
-              drag="x"
-              dragConstraints={{
-                left: window.matchMedia("(max-width: 700px)").matches
-                  ? -285.066666 * (repos.length - 1)
-                  : -570.133333 * (repos.length - 1),
-                right: window.matchMedia("(max-width: 700px)").matches
-                  ? 285.066666 * (repos.length - 1)
-                  : 570.133333 * (repos.length - 1),
-              }}
-            >
-              {repos.map((repo) => (
-                <figure key={repo.id}>
-                  <Image
-                    src={`https://raw.githubusercontent.com/AndreyBV5/${repo.name}/main/banner.png`}
-                    placeholder="blur"
-                    alt={repo.name}
-                    width={500}
-                    height={300}
-                  />
-                </figure>
-              ))}
-            </motion.div>
-            <div>
-              <button type="button" onClick={() => handleSlide("right")}>
-                <svg
-                  width={15}
-                  height={16}
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M8 0 0 8l8 8 1.687-1.687-5.121-5.12h10.241V6.807H4.566l5.12-5.12L8 0Z"
-                    fill="#fff"
-                  />
-                </svg>
-              </button>
-              <button type="button" onClick={() => handleSlide("left")}>
-                <svg
-                  width={15}
-                  height={16}
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-
-                    d="m6.807 16 8-8-8-8-1.686 1.687 5.12 5.12H0v2.386h10.241l-5.12 5.12L6.807 16Z"
-                    fill="#fff"
-                  />
-                </svg>
-              </button>
-            </div>
-          </section>
+        <section className={styles.work}>
+          <h2>My Work</h2>
+          <motion.div
+            animate={{ x }}
+            drag="x"
+            dragConstraints={{ left: -855.2, right: 855.2 }}
+          >
+            <figure>
+              <Image src={One} alt="" width={500} height={300}/>
+            </figure>
+            <figure>
+              <Image src={Two} alt="" width={500} height={300}/>
+            </figure>
+            <figure>
+              <Image src={Three} alt="" width={500} height={300}/>
+            </figure>
+            <figure>
+              <Image src={Four} alt="" width={500} height={300}/>
+            </figure>
+            <figure>
+              <Image src={Five} alt="" width={500} height={300}/>
+            </figure>
+          </motion.div>
+          <div>
+            <button type="button" onClick={() => handleSlide("right")}>
+              <svg
+                width={15}
+                height={16}
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8 0 0 8l8 8 1.687-1.687-5.121-5.12h10.241V6.807H4.566l5.12-5.12L8 0Z"
+                  fill="#fff"
+                />
+              </svg>
+            </button>
+            <button type="button" onClick={() => handleSlide("left")}>
+              <svg
+                width={15}
+                height={16}
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m6.807 16 8-8-8-8-1.686 1.687 5.12 5.12H0v2.386h10.241l-5.12 5.12L6.807 16Z"
+                  fill="#fff"
+                />
+              </svg>
+            </button>
+          </div>
         </section>
       </div>
     </>

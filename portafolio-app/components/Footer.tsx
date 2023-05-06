7d4d2329435ba8192a8baaac3b9061d198d453cd
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-05 18:34:54 -0600

    Footer y boostrap
    creacion de footer y instalacion de boostrap

---
+++ portafolio-app/components/Footer.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from "../styles/Home.module.css"
+import Image from 'next/image'
+import Logo from "../public/Logo Andrey.png"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <div>
+          <article>
+            <h2>Book a call with me</h2>
+            <p className={styles.desktop}>
+              I’d love to have a chat to see how I can help you. The best first
+              <br/>
+              step is for us to discuss your project during a free consultation.
+              <br/>
+              Then we can move forward from there.
+            </p>
+          </article>
+          <button type="button">Free Consultation</button>
+        </div>
+        <nav className={styles.nav}>
+          <button type="button">
+            <Image src={Logo} alt=""/>
+          </button>
+          <button type="button">Free Consultation</button>
+        </nav>
+      </footer>
+  )
+}
+
+export default Footer
--- portafolio-app/pages/index.tsx
+++ portafolio-app/pages/index.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Projects from '@/components/Projects'
+import Footer from '@/components/Footer'
 
 
 
@@ -22,6 +23,7 @@ const Home: NextPage = () =>{
       <Hero/>
       <About/>
       <Projects/>
+      <Footer/>
 
 
 

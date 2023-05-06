f012454fdab4cfa0f6f49b37b22f540e663e1603
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-06 09:33:24 -0600

    uptade

--- portafolio-app/components/Projects.tsx
+++ portafolio-app/components/Projects.tsx
@@ -1,118 +1,102 @@
-import React from "react";
-import { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import styles from "../styles/Home.module.css"
-
-
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 const Projects = () => {
   type Repo = {
-
     id: number;
-    html_url: string;
     name: string;
-    image_url: string;
   }
-  const [repos, setRepos] = useState<Repo[]>([]);
   const [x, setX] = useState(0);
+  const [repos, setRepos] = useState<Repo[]>([]);
 
   const handleSlide = (direction: any) => {
-    if (window.matchMedia('(max-width: 700px)').matches) {
-      if (direction === 'left') {
-        setX(x - 285.066666);
-      } else {
-        setX(x + 285.066666);
-      }
+    const slideWidth = window.matchMedia("(max-width: 700px)").matches
+      ? 285.066666
+      : 570.133333;
+    if (direction === "left") {
+      setX(x - slideWidth);
     } else {
-      if (direction === 'left') {
-        setX(x - 570.133333);
-      } else {
-        setX(x + 570.133333);
-      }
+      setX(x + slideWidth);
     }
   };
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://api.github.com/user/repos', {
-        headers: {
-          Authorization: `Bearer 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`,
-        },
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setRepos(data);
-      }
+      const response = await fetch(
+        "https://api.github.com/users/AndreyBV5/repos"
+      );
+      const data = await response.json();
+      setRepos(data);
     };
-
     fetchData();
   }, []);
 
   useEffect(() => {
-    if (window.matchMedia('(max-width: 700px)').matches) {
-      if (x > 570.133333) {
-        setX(570.133333);
-      }
-      if (x < -570.133333) {
-        setX(-570.133333);
-      }
-    } else {
-      if (x > 1140.26667) {
-        setX(1140.26667);
-      }
-      if (x < -1140.26667) {
-        setX(-1140.26667);
-      }
+    const slideWidth = window.matchMedia("(max-width: 700px)").matches
+      ? 285.066666
+      : 570.133333;
+    if (x > slideWidth * (repos.length - 1)) {
+      setX(slideWidth * (repos.length - 1));
     }
-  }, [x]);
-
+    if (x < -slideWidth * (repos.length - 1)) {
+      setX(-slideWidth * (repos.length - 1));
+    }
+  }, [x, repos]);
   return (
     <>
       <div className={styles.desktop}>
-        <section id='Projects'>
+        <section id="Projects">
           <section className={styles.work}>
             <h2>My Projects</h2>
             <motion.div
               animate={{ x }}
-              drag='x'
-              dragConstraints={{ left: -855.2, right: 855.2 }}
+              drag="x"
+              dragConstraints={{
+                left: window.matchMedia("(max-width: 700px)").matches
+                  ? -285.066666 * (repos.length - 1)
+                  : -570.133333 * (repos.length - 1),
+                right: window.matchMedia("(max-width: 700px)").matches
+                  ? 285.066666 * (repos.length - 1)
+                  : 570.133333 * (repos.length - 1),
+              }}
             >
-              <div className={styles.projectsContainer}>
-                {repos.map((repo) => (
-                  <figure key={repo.id} className={styles.project}>
-                    <a href={repo.html_url} target='_blank' rel='noreferrer'>
-                      <div className={styles.imageContainer}>
-                        <img src={repo.image_url} alt={`${repo.name} logo`} />
-                      </div>
-                      <div className={styles.name}>{repo.name}</div>
-                    </a>
-                  </figure>
-                ))}
-              </div>
+              {repos.map((repo) => (
+                <figure key={repo.id}>
+                  <Image
+                    src={`https://raw.githubusercontent.com/AndreyBV5/${repo.name}/main/banner.png`}
+                    placeholder="blur"
+                    alt={repo.name}
+                    width={500}
+                    height={300}
+                  />
+                </figure>
+              ))}
             </motion.div>
             <div>
-              <button type='button' onClick={() => handleSlide('right')}>
+              <button type="button" onClick={() => handleSlide("right")}>
                 <svg
                   width={15}
                   height={16}
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    d='M8 0 0 8l8 8 1.687-1.687-5.121-5.12h10.241V6.807H4.566l5.12-5.12L8 0Z'
-                    fill='#fff'
+                    d="M8 0 0 8l8 8 1.687-1.687-5.121-5.12h10.241V6.807H4.566l5.12-5.12L8 0Z"
+                    fill="#fff"
                   />
                 </svg>
               </button>
-              <button type='button' onClick={() => handleSlide('left')}>
+              <button type="button" onClick={() => handleSlide("left")}>
                 <svg
                   width={15}
                   height={16}
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
+
                     d="m6.807 16 8-8-8-8-1.686 1.687 5.12 5.12H0v2.386h10.241l-5.12 5.12L6.807 16Z"
                     fill="#fff"
                   />
